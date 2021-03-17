2ee13941d20188757e06b83016b313797f74c038
Karim <nogipx@gmail.com>
2021-03-17 09:03:00 +0300

    Fix parser shadowing unknown route

--- lib/src/parser.dart
+++ lib/src/parser.dart
@@ -6,6 +6,7 @@ import 'package:uri/uri.dart';
 
 class AppRouteInformationParser extends RouteInformationParser<AppRoute> {
   final List<AppRoute> routes;
+  final AppRoute initialRoute;
   final AppRoute unknownRoute;
   final Uri Function(Uri initialUri) transformUri;
   final Function(Uri uri, AppRoute selectedRoute) onExternalRoute;
@@ -13,26 +14,44 @@ class AppRouteInformationParser extends RouteInformationParser<AppRoute> {
   AppRouteInformationParser({
     @required this.routes,
     @required this.unknownRoute,
+    this.initialRoute,
     this.transformUri,
     this.onExternalRoute,
   });
 
   @override
   Future<AppRoute> parseRouteInformation(RouteInformation routeInformation) async {
-    final _initial = Uri.parse(routeInformation.location);
-    final _uri = transformUri?.call(_initial) ?? _initial;
-    final route = routes.firstWhere(
-      (route) => UriParser(route.uriTemplate).matches(_uri),
-      orElse: () {
+    final _initialUri = Uri.parse(routeInformation.location);
+    final _uri = transformUri?.call(_initialUri) ?? _initialUri;
+
+    if (routeInformation.location == "/") {
+      return initialRoute ??
+          routes.singleWhere((e) => e.template == "/", orElse: () {
+            dev.log("No initial route provided.");
+            return unknownRoute;
+          });
+    } else {
+      final routesWithoutInitial =
+          routes.where((e) => e.template != "/" && e != initialRoute).toList();
+      final matchedRoutes = routesWithoutInitial
+          .where((e) => UriParser(e.uriTemplate).matches(_uri))
+          .toList();
+      if (matchedRoutes.isEmpty) {
         dev.log(
-          "No route for path from system: '$_uri'",
+          "No match for system route: '$_uri'",
           name: runtimeType.toString(),
         );
         return unknownRoute;
-      },
-    );
-    final _route = route.fill(rawData: UriParser(route.uriTemplate).parse(_uri));
-    onExternalRoute?.call(_uri, _route);
-    return _route;
+      } else {
+        if (matchedRoutes.length > 1) {
+          dev.log(
+            "Few routes(${matchedRoutes.length}) has matched to '$_uri'. Last will be used.",
+            name: runtimeType.toString(),
+          );
+        }
+        final route = matchedRoutes.last;
+        return route.fill(rawData: UriParser(route.uriTemplate).parse(_uri));
+      }
+    }
   }
 }
